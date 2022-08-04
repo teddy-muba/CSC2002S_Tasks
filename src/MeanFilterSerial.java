9e728a5977902d5dc8dcbf3389d4a6ed9a95ad3c
teddy-muba <tddmub001@myuct.ac.za>
2022-08-04 10:24:46 +0200

    my commit test

--- src/MeanFilterSerial.java
+++ src/MeanFilterSerial.java
@@ -1,5 +1,10 @@
 public class MeanFilterSerial {
     public static void main(String[] args) {
         System.out.println("this is my test");
+        System.out.println("another test");
+        System.out.println("this is my test");
+        System.out.println("another test");
+        System.out.println("this is my test");
+        System.out.println("another test");
     }
 }
